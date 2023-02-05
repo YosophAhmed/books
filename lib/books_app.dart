85486f29876016d234dbb700f9cf671d6855777a
Yosoph Ahmed <devyosoph@gmail.com>
2023-02-05 17:55:18 +0200

    Splash View Body

--- lib/books_app.dart
+++ lib/books_app.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'features/splash/presentation/views/splash_view.dart';
+
 class BooksApp extends StatelessWidget {
   const BooksApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return const GetMaterialApp(
       home: SplashView(),
     );
   }
---
+++ lib/features/splash/presentation/widgets/splash_view_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Column(
+      children: [],
+    );
+  }
+}
