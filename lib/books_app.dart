bfc756e7e6def3ba54c265ea64e3b816b2f97ce0
Yosoph Ahmed <devyosoph@gmail.com>
2023-02-07 20:25:21 +0200

    Latest Commit

--- lib/books_app.dart
+++ lib/books_app.dart
@@ -1,6 +1,7 @@
 import 'package:books/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import 'features/splash/presentation/views/splash_view.dart';
 
@@ -9,11 +10,14 @@ class BooksApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  GetMaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       home: const SplashView(),
       theme: ThemeData.dark().copyWith(
         scaffoldBackgroundColor: kPrimaryColor,
+        textTheme: GoogleFonts.montserratTextTheme(
+          ThemeData.dark().textTheme,
+        ),
       ),
     );
   }
--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:books/core/utils/styles.dart';
 import 'package:books/features/home/presentation/views/widgets/custom_app_bar.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 import 'books_list_view.dart';
@@ -24,6 +25,51 @@ class HomeViewBody extends StatelessWidget {
           Text(
             'Best Seller',
             style: Styles.mediumTitle,
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          BestSellerItem(
+            url:
+                'https://edit.org/photos/images/cat/book-covers-big-2019101610.jpg-1300.jpg',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerItem extends StatelessWidget {
+  final String url;
+
+  const BestSellerItem({
+    Key? key,
+    required this.url,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 150,
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(16),
+            child: AspectRatio(
+              aspectRatio: 2.6 / 4,
+              child: CachedNetworkImage(
+                fit: BoxFit.fill,
+                imageUrl: url,
+                errorWidget: (
+                  context,
+                  url,
+                  error,
+                ) =>
+                    const Icon(
+                  Icons.error,
+                ),
+              ),
+            ),
           ),
         ],
       ),
