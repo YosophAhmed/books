524862199fa24bc602f872ab00804150abf048d9
Yosoph Ahmed <devyosoph@gmail.com>
2023-02-13 00:20:55 +0200

    Featured Books Cubit

--- lib/features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart
@@ -1,8 +1,32 @@
+import 'package:books/features/home/data/repos/home_repo.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  final HomeRepo homeRepo;
 
+  FeaturedBooksCubit(
+    this.homeRepo,
+  ) : super(FeaturedBooksInitial());
 
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failure) {
+        emit(
+          FeaturedBooksError(
+            errorMessage: failure.failMessage,
+          ),
+        );
+      },
+      (books) {
+        emit(
+          FeaturedBooksSuccess(
+            books: books,
+          ),
+        );
+      },
+    );
+  }
 }
